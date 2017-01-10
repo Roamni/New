bae7d250d0a1e693342c2bfc104e77c0584b2e07
JeremyWangAU <zihaowang@zihaos-macbook-pro.local>
2017-01-10 22:59:54 +1100

    1.4

--- Roamni/MyRoamniViewController.swift
+++ Roamni/MyRoamniViewController.swift
@@ -7,47 +7,141 @@
 //
 
 import UIKit
-import FBSDKLoginKit
-import Firebase
-class MyRoamniViewController: UIViewController, FBSDKLoginButtonDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class MyRoamniViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+         self.tableView.dataSource = self
+        self.tableView.delegate = self
+        //DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async(execute: { () -> Void in
+        self.tableView.reloadData()
+            
+        //})
         
-        let loginButton = FBSDKLoginButton()
-        view.addSubview(loginButton)
-        loginButton.frame = CGRect(x:16,y:50,width:view.frame.width - 32, height:50)
-        loginButton.delegate = self
-        // Do any additional setup after loading the view.
-    }
-
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        print("log out of facebook")
+        
     }
     
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        if error != nil{
-        print(error)
-            return
-        }
-        let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
-        
-        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
-            // ...
-            if let error = error {
-                print(error)
-                return
-        }
-        }
-        print("successfully logged in ")
-    }
-        
-        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return 2 // your number of cell here
+        switch(section)
+        {
+        case 0: return 1
+        case 1: return 1
+        case 2: return 1
+        case 3: return 1
+        case 4: return 1
+        case 5: return 1
+        case 6: return 1
+        default: return 0
+        }
+        
+    }
+    func numberOfSections(in tableView: UITableView) -> Int{
+        // #warning Incomplete implementation, return the number of sections
+        return 7
+    }
+
+//    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+//        return 0.0000001
+//    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        
+        if indexPath.section == 0{
+            return 120.0//Choose your custom row height
+        }
+        if indexPath.section == 1{
+            return 50.0//Choose your custom row height
+        }
+        if indexPath.section == 2{
+            return 50.0//Choose your custom row height
+        }
+        if indexPath.section == 3{
+            return 50.0//Choose your custom row height
+        }
+        if indexPath.section == 4{
+            return 50.0//Choose your custom row height
+        }
+        if indexPath.section == 5{
+            return 50.0//Choose your custom row height
+        }
+        if indexPath.section == 6{
+            return 50.0//Choose your custom row height
+        }
+     
+        
+        
+        return 100.0
+    }
+    
+    //This function returns a cell for a table view, the returned object is of
+    //type UITableViewCell. These are the objects that users see in the table's rows.
+    //This function basically returns a cell, for a table view.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0{
+            //Return the cell with identifier NotificationTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniUserCell", for: indexPath as IndexPath) as! MyRoamniUserCell
+            return cell
+        }
+        else if indexPath.section == 1{
+            //Return the cell with identifier BackTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniRequestTourCell", for: indexPath as IndexPath) as! MyRoamniRequestTourCell
+            return cell
+            
+        }else if indexPath.section == 2{
+            //Return the cell with identifier AboutTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniDownloadCell", for: indexPath as IndexPath) as! MyRoamniDownloadCell
+            return cell
+            
+            
+        }else if indexPath.section == 3{
+            //Return the cell with identifier AboutTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniUploadTourCell", for: indexPath as IndexPath)
+                as! MyRoamniUploadTourCell
+            return cell
+            
+            
+        }else if indexPath.section == 4{
+            //Return the cell with identifier AboutTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniDistributionCell", for: indexPath as IndexPath)
+                as! MyRoamniDistributionCell
+            return cell
+            
+            
+        }else if indexPath.section == 5{
+            //Return the cell with identifier AboutTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniHowCell", for: indexPath as IndexPath)
+                as! MyRoamniHowCell
+            return cell
+            
+            
+        }else {
+            //Return the cell with identifier AboutTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniAboutCell", for: indexPath as IndexPath)
+                as! MyRoamniAboutCell
+            return cell
+            
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //        if indexPath.section == 3{
+       // performSegue(withIdentifier: "searchSegue", sender: self)
+        //        }
+    }
+
+
+
 
     /*
     // MARK: - Navigation
